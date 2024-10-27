216a4092ce5566254e01de3de6f1ea102c8986b0
HrishikeshSardar2006 <hrishikeshsardar65@gmail.com>
2024-10-27 21:32:43 +0530

    Add files via upload
    It's simple Calculator which is built in C Language

---
+++ cal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char op;
+    float a, b;
+    printf("Enter The First Number\n");
+    scanf("%d", &a);
+    printf("Enter The Operator(+, -, *, /)\n");
+    scanf(" %c", &op);
+    printf("Enter The Second Number\n");
+    scanf("%d", &b);
+    switch (op)
+    {
+    case '+':
+        printf("%f + %f = %f", a, b, a + b);
+        break;
+    case '-':
+        printf("%f - %f = %f", a, b, a - b);
+        break;
+    case '*':
+        printf("%f * %f = %f", a, b, a * b);
+        break;
+    case '/':
+        printf("%f / %f = %f", a, b, a / b);
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
